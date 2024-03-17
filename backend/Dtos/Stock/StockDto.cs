37d66987d4b5fdf473769181037b4f0614efd248
unknown <gagas112212@gmail.com>
2024-03-17 17:00:33 +0700

    comment class

--- backend/Dtos/Stock/StockDto.cs
+++ backend/Dtos/Stock/StockDto.cs
@@ -18,6 +18,6 @@ namespace backend.Dtos.Stock
         public decimal LastDiv { get; set; }
         public string Industry { get; set; } = string.Empty;
         public long MarketCap { get; set; }
-        //Comments
+        //Comment
     }
 }
